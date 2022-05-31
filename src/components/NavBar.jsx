e12f3eec7bc41b86095bad46038274a4c25af771
Nico Velazquez <nicovege@gmail.com>
2022-05-30 23:40:00 -0300

    navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function NavBar() {
+    return (
+        <>
+        <div>
+            <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                <img className="caps" src="./img/caps.png" alt="caps" />
+                <strong> hAtch CaPs</strong>
+                </Navbar.Brand>
+                <Nav className="ms-auto">
+                <Nav.Link href="#home">Products </Nav.Link>
+                <Nav.Link href="#features">Category </Nav.Link>
+                </Nav>
+            </Container>
+            </Navbar>
+        </div>
+        </>
+    )
+}
+
+export default NavBar;
